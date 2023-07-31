6d12f7e5a3b2aac9b1d6264dee5a41b4355c9303
jagdishkumawat07 <jagdishkumawat07@gmail.com>
2023-07-31 17:08:15 +0530

    update code text

--- src/main/java/com/example/lowertoupperCase/controller/LowerToUpperStringController.java
+++ src/main/java/com/example/lowertoupperCase/controller/LowerToUpperStringController.java
@@ -9,7 +9,7 @@ public class LowerToUpperStringController {
 
 	@GetMapping("/{name}")
 	public String getString(@PathVariable String name) {
-		name = name.toUpperCase()+" sharmaji";
+		name = name.toUpperCase()+" sharmaji Kumar";
 		return name;
 	}
 }
